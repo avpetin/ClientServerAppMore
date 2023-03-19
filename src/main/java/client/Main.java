b9e8e63f509dac68ad9d016e9aafa7256f15daf7
Alexey Petin <alex_beholder@mail.ru>
2023-03-19 22:49:44 +0300

    Update1

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -5,27 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String host = "netology.homework";
         int port = 8090;
 
-        try (Socket clientSocket = new Socket(host, port);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-        ) {
-
-            String resp = in.readLine();
-            if(resp.equals("Write your name")) {
-                out.println("Netology");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try (Socket clientSocket = new Socket(host, port);
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                String resp = in.readLine();
+                if (resp.equals("Write your name")) {
+                    System.out.println(resp);
+                    String userResp = scanner.nextLine();
+                    out.println(userResp);
+                } else if (resp.equals("Are you child? (yes/no)")) {
+                    System.out.println(resp);
+                    String userResp = scanner.nextLine();
+                    out.println(userResp);
+                    break;
+                }
+            }catch(IOException e){
+                e.printStackTrace();
             }
-            else if(resp.equals("Are you child? (yes/no)")){
-                out.println("no");
-            }
-            System.out.println(resp);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -10,6 +10,9 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         int port = 8090;
+        String name = null;
+        String isChild;
+        int step = 0;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
@@ -18,8 +21,32 @@ public class Main {
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 ) {
                     System.out.println("New connection accepted");
-                    final String name = in.readLine();
-                    out.println("Write your name");
+                    switch (step){
+                        case 0:
+                            out.println("Write your name");
+                            step++;
+                            break;
+                        case 1:
+                            name = in.readLine();
+                            if(name != null){
+                                out.println("Are you child? (yes/no)");
+                                step++;
+                            }
+                            break;
+                        case 2:
+                            isChild = in.readLine();
+                            if(isChild != null){
+                                if(isChild.equals("yes")) {
+                                    out.println("Welcome to the kids area, " + name + "! Let's play!");
+                                }
+                                else if(isChild.equals("no")) {
+                                    out.println(String.format("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!"));
+                                }
+                                step++;
+                            }
+                            break;
+                    }
+
                 }
             }
         }
